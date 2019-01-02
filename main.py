6495066f49abadfc6d605fbcc3ebb7df4786f2da
Joe Betti <joeb676@gmail.com>
2019-01-01 23:57:50 -0500

    h

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 import requests
+from bs4 import BeautifulSoup
 
-url = "https://glock.photo/yidi"
-r = requests(url)
+#url = 'https://www.google.com'
+url = "https://isthereanydeal.com"
+r = requests.get(url)
+r.text
 print(r.content[:100])
 
-def saveHTML(html,path):
-    with open(path,"wb") as f:
+def saveHTML(html, path):
+    with open(path, 'wb') as f:
         f.write(html)
 #saves html from webpage to text file "f"
 
-saveHTML(r.content, "google.com")
+saveHTML(r.content, 'google_com')
 
 def openHTML(path):
-    with open(path, "rb") as f:
+    with open(path, 'rb') as f:
         return f.read()
+    
+html = openHTML('google_com')
 
-html = openHTML("google.com")
+soup = BeautifulSoup(r.content,'html.parser')
+
+gameTitles = soup.select('div.title')
+gameTitle = gameTitles[0]
+
+title = gameTitle.select_one('div.title').text.strip()
+print(title)
